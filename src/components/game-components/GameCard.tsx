8cbba1804415cc4b181b7e32b6a7051decc6af6b
Emad Lahham <emad.la271419@gmail.com>
2023-04-09 08:33:48 +0300

    Clean up the game card

--- src/components/game-components/GameCard.tsx
+++ src/components/game-components/GameCard.tsx
@@ -11,16 +11,19 @@ interface Props {
 
 const GameCard = ({game}: Props) => {
     return (
-        <Card >
+        <Card>
             <Image src={getCroppedImageUrl(game.background_image)}/>
             <CardBody>
+                <HStack
+                    justifyContent='space-between'
+                    marginBottom={3}
+                >
+                    <PlatformIconList key={game.id} platforms={game.parent_platforms.map(p => p.platform)}/>
+                    <CriticScore score={game.metacritic}/>
+                </HStack>
                 <Heading fontSize="2xl">
                     {game.name}
                 </Heading>
-                <HStack justifyContent='space-between'>
-                    <PlatformIconList key={game.id} platforms={game.parent_platforms.map(p=> p.platform)}/>
-                    <CriticScore score={game.metacritic}/>
-                </HStack>
             </CardBody>
         </Card>
     );
